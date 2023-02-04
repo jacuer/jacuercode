bdcaacb1d67d13b449ac8c49f7a949bd0ca52d0d
jacuer <107208247+jacuer@users.noreply.github.com>
2023-02-04 22:56:06 +0100

    Update Model.php

--- Model.php
+++ Model.php
@@ -2,11 +2,11 @@
 
 abstract class Model{
    
- private static   $db_host = 'localhost';
- private static   $db_user = 'root';
- private static   $db_pass = '';
- private static   $db_name = 'votacion';
- private static   $db_charset = 'utf8';
+ private $db_host;
+ private $db_user;
+ private $db_pass;
+ private $db_name;
+ private $db_charset = 'utf8';
  private $conn;
  protected $query;
  protected $rows = array();
@@ -17,17 +17,27 @@ abstract class Model{
  abstract protected function del();
 
 
+    public function __construct() {
+        require_once 'Credentials.php';
+        $credentials = new Credentials();
+
+        $this->db_host = $credentials->getHost();
+        $this->db_user = $credentials->getUser();
+        $this->db_pass = $credentials->getPassword();
+        $this->db_name = $credentials->getDatabase();
+    }
+	
     private function db_open(){
        
         $this->conn = new mysqli(
-            self::$db_host,
-            self::$db_user,
-            self::$db_pass,
-            self::$db_name
+            $this->db_host,
+            $this->db_user,
+            $this->db_pass,
+            $this->db_name
         );
 
        
-        $this->conn->set_charset(self::$db_charset);
+        $this->conn->set_charset($this->db_charset);
     }
 
 
